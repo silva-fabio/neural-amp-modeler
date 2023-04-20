e2b7db7ee315a6deff7e319d3dbc00b5aa4675e5
Fábio Silva <fabio_silva2@apple.com>
2023-04-20 03:56:06 -0300

    feat: add arg-list parameter when calling run_colab

--- bin/train/main.py
+++ bin/train/main.py
@@ -215,6 +215,13 @@ def main_inner(
         )
         plot(model, dataset_validation, show=not no_show)
 
+def to_dict(arg_list):
+    try:
+        return {name: int(str_v) if str_v.isdigit() else float(str_v) if '.' in str_v else str_v for name, str_v in (s.split('=') for s in arg_list)}
+    except Exception:
+        print(f"Error in argument list: '{arg_list}'")
+        print(" ** Example of use: -l epochs=10 architecture=standard delay=10 lr=0.004 lr_decay=0.007 seed=0")
+        return None
 
 if __name__ == "__main__":
     parser = ArgumentParser()
@@ -222,9 +229,16 @@ if __name__ == "__main__":
         parser.add_argument("--easy-colab", "-c", action="store_true", help="Run easy colab training")
         parser.add_argument("--epochs", "-e", type=int, default=100, help="Set epoch when running easy colab training")
         parser.add_argument("--arch", "-a", type=str, default="standard", help="Set architecture when running easy colab training")
+        parser.add_argument("--arg-list", "-l", type=str, default=None, nargs='*', help="Set list of parameter values, like so: -l epochs=10 architecture=standard delay=10 lr=0.004 lr_decay=0.007 seed=0")
         args = parser.parse_args()
-        print(f"* Runing easy colab: run(epochs={args.epochs}, architecture={args.arch})")
-        run_colab(epochs=args.epochs, architecture=args.arch)
+        if args.arg_list:
+            arg_list=to_dict(args.arg_list)
+            if arg_list:
+                print(f"* Runing easy colab: run({arg_list})")
+                run_colab(**arg_list)
+        else:
+            print(f"* Runing easy colab: run(epochs={args.epochs}, architecture={args.arch})")
+            run_colab(epochs=args.epochs, architecture=args.arch)
         sys.exit(0)
     parser.add_argument("data_config_path", type=str)
     parser.add_argument("model_config_path", type=str)
